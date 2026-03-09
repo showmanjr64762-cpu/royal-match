65b1b5d9703dc21944f20d898613e694a0625aa6
showmanjr64762-cpu <showmanjr64762@gmail.com>
2026-03-10 04:34:02 +0500

    Set correct Firebase database URL

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,15 @@
 const admin = require("firebase-admin");
 
-// Construct service account from environment variables
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID, // optional if you want
   private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FB_CLIENT_EMAIL,
 };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FB_DATABASE_URL,
+  databaseURL: process.env.FB_DATABASE_URL,  // <-- use environment variable
 });
 
 const db = admin.database();
