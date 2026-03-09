74cd4298aacc04b7d466093773ccb1b39b278a59
showmanjr64762-cpu <showmanjr64762@gmail.com>
2026-03-09 14:48:14 +0500

    Cleaned broken template

--- config/firebase.js
+++ config/firebase.js
@@ -1,36 +1,18 @@
-// config/firebase.js
 const admin = require("firebase-admin");
 
+let serviceAccount;
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-
-const db = admin.firestore();
-module.exports = db;
-
-
-if (!admin.apps.length) {
-  if (!process.env.FB_DATABASE_URL) {
-    console.error("❌ FB_DATABASE_URL is missing!");
-    process.exit(1); // stop server if missing
-  }
+if (process.env.FIREBASE_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FB_PROJECT_ID,
-      clientEmail: process.env.FB_CLIENT_EMAIL,
-      privateKey: process.env.FB_PRIVATE_KEY
-        ? process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n")
-        : undefined,
-    }),
-    databaseURL: process.env.FB_DATABASE_URL,
+    credential: admin.credential.cert(serviceAccount)
   });
+
+} else {
+  console.log("FIREBASE_KEY not found in environment variables");
 }
 
+const db = admin.firestore();
 
-console.log("✅ Firebase initialized successfully with DB:", process.env.FB_DATABASE_URL);
-
+module.exports = db;
